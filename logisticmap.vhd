5735cf66a5cd1ce688013296d4d67cb3fe1b2d74
Matheus C Nali <nali.matheus@gmail.com>
2019-06-04 01:41:13 -0300

    Readme updated, comments added

--- logisticmap.vhd
+++ logisticmap.vhd
@@ -10,6 +10,7 @@ entity logisticmap is
 		);
 end logisticmap;
 
+-- Declared all the entities here for faster debbuging compilation
 library ieee;
 use ieee.std_logic_1164.all;
 use work.packagefpga.all;
@@ -88,12 +89,14 @@ architecture lmap of logisticmap is
 	signal c:std_logic;
 
 begin
-
 	multiplex: multiplexer port map(x0, z, D, start);
 	flipflopd: ffd port map(clk,clr,D,Q);
 	mult: multiplier port map(Q,Q,Qsq);
+	-- Used two subtractors of 16 bits connected to make the subtraction of x(i)-x(i)^2
+	-- Since x(i)^2 has 32 bits, extended the x(i) to 32 bits filling it with zeros on the less significant part
 	sub1: n_subtractor port map( Q,Qsq(bussize*2-1 downto bussize), sub_out(bussize*2-1 downto bussize),c, open);
 	sub2: n_subtractor port map((others =>'0'),Qsq(bussize-1 downto 0), sub_out(bussize-1 downto 0), '0', c);
+	-- leftshift <<2 takes the previous 32 bits subtraction and shift it then take the 16 most significant bits
 	shift: shift2 port map(sub_out, z);
 
 	xnout <=z;
